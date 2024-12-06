24b74abc22a2bf013b2182d37c5d1c517a406501
Callum.White <callum.white@partners-cap.com>
2024-12-06 12:02:17 +0000

    day 6 - not happy with this......

---
+++ soluations/day_6.py
@@ -0,0 +1,121 @@
+from util.input import get_input_as_list
+
+# ....#.....
+# .........#
+# ..........
+# ..#.......
+# .......#..
+# ..........
+# .#..^.....
+# ........#.
+# #.........
+# ......#...
+
+if __name__ == "__main__":
+    input_data = get_input_as_list("inputs/day_6.txt")
+    matrix_data = [x[0] for x in input_data]
+    max_y = len(matrix_data)
+    max_x = len(matrix_data[0]) 
+    print(max_x, max_y)
+    
+    pos_mov = 'up'
+    
+    start_x = -1
+    start_y = -1
+    
+    obstacles=set()
+    for y in range(max_y):
+        for x in range(max_x):
+            if matrix_data[y][x] == "^":
+                start_x = x
+                start_y = y
+            if matrix_data[y][x] == "#":
+                obstacles.add((x, y))
+    
+    cur_x, cur_y = start_x, start_y
+    
+    new_obvs = set()
+    
+    unique_points = set()
+    turning_points = set()
+    
+    def is_valid(x, y):
+        return 0 <= x < max_x and 0 <= y < max_y
+    
+    pos_mov_map = {
+        "up": "right",
+        "right": "down",
+        "down": "left",
+        "left": "up"
+    }
+    unique_points.add((cur_x, cur_y))
+    while(is_valid(cur_x, cur_y)):
+        if pos_mov == "up":
+            if matrix_data[cur_y][cur_x] == "#":
+                pos_mov = pos_mov_map[pos_mov]
+                obstacles.add((cur_x, cur_y))
+                cur_y += 1
+                turning_points.add((cur_x, cur_y))
+                cur_x += 1
+                continue
+            unique_points.add((cur_x, cur_y))
+            cur_y -= 1
+        if pos_mov == "right":
+            if matrix_data[cur_y][cur_x] == "#":
+                pos_mov = pos_mov_map[pos_mov]
+                obstacles.add((cur_x, cur_y))
+                cur_x -= 1
+                turning_points.add((cur_x, cur_y))
+                cur_y += 1
+                continue
+            unique_points.add((cur_x, cur_y))
+            cur_x += 1
+        if pos_mov == "down":
+            if matrix_data[cur_y][cur_x] == "#":
+                pos_mov = pos_mov_map[pos_mov]
+                obstacles.add((cur_x, cur_y))
+                cur_y -= 1
+                turning_points.add((cur_x, cur_y))
+                cur_x -= 1
+                continue
+            unique_points.add((cur_x, cur_y))
+            cur_y += 1
+        if pos_mov == "left":
+            if matrix_data[cur_y][cur_x] == "#":
+                pos_mov = pos_mov_map[pos_mov]
+                obstacles.add((cur_x, cur_y))
+                cur_x += 1
+                turning_points.add((cur_x, cur_y))
+                cur_y -= 1
+                continue
+            unique_points.add((cur_x, cur_y))
+            cur_x -= 1
+        
+    def is_loop(obstacles, start, max_x, max_y):
+        visited = set()
+        x, y = start
+        direction = -1j
+
+        while 0 <= x < max_x and 0 <= y < max_y:
+            if ((x, y), direction) in visited:
+                return True
+            visited.add(((x, y), direction))
+            next_pos = (x + direction.real, y + direction.imag)
+            while next_pos in obstacles:
+                direction = direction * 1j
+                next_pos = (x + direction.real, y + direction.imag)
+            x, y = next_pos
+
+        return False
+    
+    print(len(unique_points))
+    
+    g_pos = set()
+    unique_points.remove((start_x, start_y))
+    print(len(obstacles))
+    for point in unique_points:
+        obstacles.add(point)
+        if is_loop(obstacles, (start_x, start_y), max_x, max_y):
+            g_pos.add(point)
+        obstacles.remove(point)
+    print(len(g_pos))
